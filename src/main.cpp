9edcd85a27a9ba3d2969855ef5163c65a9bb2045
yuta sakamoto <u.s.kutieehmn14@gmail.com>
2015-11-28 14:08:03 +0900

    今回の開発で不要な箇所のコードを削除(コマンドラインからの入力を読み取るところ)

--- src/main.cpp
+++ src/main.cpp
@@ -7,67 +7,12 @@
 // #include "mykey.h"
 #include "jphacks_key.h"
 
-static void str_append(std::string *to, std::string const &s)
+int main(void)
 {
-    if (!to->empty()) {
-        *to += ' ';
-    }
-    *to += s;
-}
-
-int main(int argc, char **argv)
-{
-    if (argc < 2) {
-        return 1;
-    }
-
-    std::string message;
-    {
-        bool f_stdin = false;
-        bool f_base64 = false;
-        bool f_separator = false;
-        int i = 1;
-        while (i < argc) {
-            std::string arg = argv[i];
-            i++;
-            if (f_separator) {
-                str_append(&message, arg);
-            } else if (arg[0] == '-') {
-                if (arg == "--") {
-                    f_separator = true;
-                } else if (arg == "-stdin") {
-                    f_stdin = true;
-                } else if (arg == "-base64") {
-                    f_base64 = true;
-                } else {
-                    fprintf(stderr, "unknown option: %s\n", arg.c_str());
-                }
-            } else {
-                if (f_base64) {
-                    message += arg;
-                } else {
-                    str_append(&message, arg);
-                }
-            }
-        }
-        if (f_stdin) {
-            char tmp[1000];
-            int n = fread(tmp, 1, sizeof(tmp), stdin);
-            message = std::string(tmp, n);
-        }
-        if (f_base64) {
-            oauth_decode_base64((unsigned char*)message.c_str(), message.c_str());
-        }
-    }
-
-    WebClient::initialize();
-    TwitterClient tc(c_key, c_sec, t_key, t_sec);
+		WebClient::initialize();
+		TwitterClient tc(c_key, c_sec, t_key, t_sec);
     std::string filename("../../skeleton.png");
     cv::Mat src = cv::imread(filename);
 
     return ( tc.tweet(message, src) ) ? 0 : 1;
-    // return ( tc.tweet( message, cv::Mat() ) ) ? 0 : 1;
-    // return ( tc.tweet( "", src) ) ? 0 : 1;
-    // return ( tc.tweet(message, filename) ) ? 0 : 1;
-    // return ( tc.tweet( "", filename) ) ? 0 : 1;
 }
